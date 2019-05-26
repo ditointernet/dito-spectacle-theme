ddf8448c346cefb992e7a1972503b71a13e64c55
Uriell <uriellarttusviana+github@gmail.com>
2019-05-26 14:06:48 -0300

    i fucked up the index

--- slides/index.js
+++ slides/index.js
@@ -1,17 +1,3 @@
-import React from "react";
-import screen from "./screen";
-import print from "spectacle/lib/themes/default/print";
-import { Deck as SpectacleDeck } from "spectacle";
-
-export const createTheme = () => ({ screen: screen(), print: print() });
-
-export const Deck = ({ progress, ...props }) => (
-  <SpectacleDeck
-    showFullscreenControl={true}
-    {...props}
-    progress={progress ? "bar" : "none"}
-    controls={false}
-    theme={createTheme()}
-    transitionDuration={200}
-  />
-);
+export { default as Cover } from "./cover";
+export { default as Impact } from "./impact";
+export { default as Content } from "./content";
