7ace7d396dab5499b874a182183d8767a335f8ef
Uriell <uriellarttusviana+github@gmail.com>
2019-05-26 13:02:47 -0300

    components index and apply prop util

--- components/Heading.js
+++ components/Heading.js
@@ -2,20 +2,20 @@ import styled from "styled-components";
 import { Heading as SpectacleHeading } from "spectacle";
 
 import COLORS from "../colors";
+import { prop } from "../utils/functional";
 
 const Heading = styled(SpectacleHeading)`
   font-weight: ${props => props.fontWeight + " !important"};
 
   &:after {
     font-weight: 600;
-    ${props => (props.suffix ? `content: '${props.suffix}';` : "")}
     color: ${props => props.suffixColor || COLORS.NEW.GREEN_500};
+    content: '${prop("suffix", "")}';
   }
 
   strong {
     font-weight: 800;
-    ${props =>
-      props.boldTextSize ? `font-size: '${props.boldTextSize}';` : ""}
+    font-size: ${prop("boldTextSize", "inherit")};
   }
 `;
 
