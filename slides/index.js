99b35ba3a8a05e2dc9befbd786d7a0493c245d6a
Uriell <uriellarttusviana+github@gmail.com>
2019-05-26 14:03:29 -0300

    content slide type

--- slides/index.js
+++ slides/index.js
@@ -1,2 +1,17 @@
-export { default as Cover } from "./cover";
-export { default as Impact } from "./impact";
+import React from "react";
+import screen from "./screen";
+import print from "spectacle/lib/themes/default/print";
+import { Deck as SpectacleDeck } from "spectacle";
+
+export const createTheme = () => ({ screen: screen(), print: print() });
+
+export const Deck = ({ progress, ...props }) => (
+  <SpectacleDeck
+    showFullscreenControl={true}
+    {...props}
+    progress={progress ? "bar" : "none"}
+    controls={false}
+    theme={createTheme()}
+    transitionDuration={200}
+  />
+);
