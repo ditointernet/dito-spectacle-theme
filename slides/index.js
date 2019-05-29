e2c3f9046fca9e967910c91a56af103ba325203b
Arthur Bailão <arthurbailao@gmail.com>
2019-05-29 15:05:49 -0300

    removing files

--- slides/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Cover } from "./cover";
-export { default as Impact } from "./impact";
-export { default as Content } from "./content";
-export { default as Separator } from "./separator";
