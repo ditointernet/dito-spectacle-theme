133095f3fc0d6347905aca4d5eb3a5e473ecae5d
Uriell <uriellarttusviana+github@gmail.com>
2019-06-02 16:54:23 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as COLORS from "./colors";
+import COLORS from "./colors";
 import { Bar, Heading, Lists, Deck } from "./components";
 import { Content, Cover, Impact, Separator } from "./slides";
 
